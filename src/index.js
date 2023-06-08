81060a8b81565f3d196307eaf9e623ede32be946
Josiah Sanchez <79384344+josiah-sanchez@users.noreply.github.com>
2023-06-08 14:44:16 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, {StrictMode} from 'react';
+import ReactDOM ,{createRoot} from 'react-dom/client';
+import './styles.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
